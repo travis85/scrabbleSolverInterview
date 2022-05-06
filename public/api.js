14a8ddc135bfaf33c1d16c05238b2b34ee97eea7
travis flake <tflake83@gmail.com>
2022-05-06 15:25:56 -0400

    problem solved

--- public/api.js
+++ public/api.js
@@ -2,24 +2,27 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express()
-const { ScrabbleMatch } = require('./scrabble.js')
+// const url = require('url');
+const { ScrabbleMatch } = require('./scrabble.js');
 
 
-app.use(bodyParser.urlencoded())// for endpoints
+app.use(bodyParser.urlencoded({extended: true}))// for endpoints
 // app.use(js.scrabbleMatch())
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, './index.html'));
 });
-
-app.get('/scrabble', (req, res) => {
-  const letters = req.query.q
-  res.send(ScrabbleMatch(letters))
-})
 app.get('/sowpods',(req, res)=>{
-  res.sendFile(path.join(__dirname, '/sowpods.txt'));
+  res.sendFile(path.join(__dirname, './sowpods.txt'));
 
 })
+
+app.get('/scrabble', async (req, res) => {
+
+  const letters = req.query.q
+  response = await ScrabbleMatch(letters)
+  res.send(response)
+});
 
 
 app.listen('3000', () => {
--- public/scrabble.js
+++ public/scrabble.js
@@ -2,13 +2,16 @@
 //load from url fetch intead fs..
 // const fs = require('fs')
 // const sowpods = fs.readFileSync('./sowpods.txt','utf-8');
- import 'regenerator-runtime/runtime';
+//  const runtime = require('regenerator-runtime/runtime');
 const axios = require('axios');
+// const url = require('url');
 
 exports.ScrabbleMatch = async function(randomString){
     // const word = sowWords
-    const sowpods = await axios.get(`/sowpods.txt`)
-    const word = sowpods.split('\n');
+    const sowpods = await axios.get("http://localhost:3000/sowpods")
+
+    // const sowpods = await axios.get(`${process.env.BASE_URL}/sowpods`) // use dotenv in the future :) 
+    const word = sowpods.data.split('\n');
 
     const splitString = randomString.toUpperCase();
     let words = []
@@ -17,6 +20,7 @@ exports.ScrabbleMatch = async function(randomString){
             words.push(word[i])
         }
     }
+
     function isWordValid(validLetters, attemptedWord) {
         const validLettersSplitted = validLetters.split("")
         const attemptedWordSplitted = attemptedWord.split("")
@@ -76,8 +80,9 @@ exports.ScrabbleMatch = async function(randomString){
     for (let i = 0; i < validWords.length; i++){
         addedScores.push([validWords[i],scores(validWords[i]) - scores(Blank(randomString,validWords[i]))])
     }
-    return addedScores    
+    // addedScores = JSON.stringify(addedScores.flat())  
+    console.log(addedScores)
+    return addedScores
 }
-
 
     
